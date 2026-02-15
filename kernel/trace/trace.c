2ff28558f50bf4d10c2b9ad0a844bc973fe80ffd
agostino64 <gutierrezaverruz0@hotmail.com>
2026-02-15 14:42:13 +0800

    tracing: Disable `trace_printk` if it is turned off * Even though it's no longer in use, it continues to initialize. Remove it from the root if `CONFIG_DISABLE_TRACE_PRINTK=y`
    Signed-off-by: aarch64 <gutierrezaverruz0@hotmail.com>
    Signed-off-by: Xirudly <734428735@qq.com>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -2846,6 +2846,8 @@ static void put_trace_buf(void)
 	this_cpu_dec(trace_percpu_buffer->nesting);
 }
 
+#ifdef CONFIG_DISABLE_TRACE_PRINTK
+#else
 static int alloc_percpu_trace_buffer(void)
 {
 	struct trace_buffer_struct __percpu *buffers;
@@ -2857,11 +2859,15 @@ static int alloc_percpu_trace_buffer(void)
 	trace_percpu_buffer = buffers;
 	return 0;
 }
+#endif
 
 static int buffers_allocated;
 
 void trace_printk_init_buffers(void)
 {
+#ifdef CONFIG_DISABLE_TRACE_PRINTK
+	buffers_allocated = 0;
+#else
 	if (buffers_allocated)
 		return;
 
@@ -2898,6 +2904,7 @@ void trace_printk_init_buffers(void)
 	 */
 	if (global_trace.trace_buffer.buffer)
 		tracing_start_cmdline_record();
+#endif
 }
 
 void trace_printk_start_comm(void)
